16503e2ac5e2cf07282e8fbe8b17c1812e64e3d5
Cristian100609 <casanchez28@espe.edu.ec>
2025-04-25 10:45:13 -0500

    Cambios suma cristian

---
+++ calculadora1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+void suma() {
+    cout << "Esta es una función de suma." << endl;
+}
+
+int main() {
+    suma();
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
