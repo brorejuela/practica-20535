dd601b4a145d14822328af846cb0ce4cb7e2097e
Ezequeil11s <aesaguano@espe.edu.ec>
2025-04-25 10:49:19 -0500

    ezequielcambios

---
+++ calculadora1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+
+double dividir(double a, double b) {
+    if (b == 0) {
+        cout<< "Error: No se puede dividir entre cero." << endl;
+        return 0;
+    }
+    return a / b;
+
+    return 0;
+}
