00f9241b42acf699fc48589318dd5e589775428c
brorejuela <brorejuela@espe.edu.ec>
2025-04-25 10:11:25 -0500

    cambios

--- calculadora.cpp
+++ calculadora.cpp
@@ -9,10 +9,10 @@ double multiplicar(double a, double b) {
 int main() {
     double num1, num2;
 
-    cout << "Introduce el primer número: ";
+    cout << "Introduce el primer numero: ";
     cin >> num1;
 
-    cout << "Introduce el segundo número: ";
+    cout << "Introduce el segundo numero: ";
     cin >> num2;
 
     double resultado = multiplicar(num1, num2);
